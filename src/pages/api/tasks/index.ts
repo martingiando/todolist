27868ef486d2f301c63654fbb5b8b3892cd1998d
Martin Giando <martingiando@gmail.com>
2025-05-04 16:02:45 -0300

    fix: add cors

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -1,15 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import dbConnect from "@/lib/mongodb";
 import Task from "@/models/Task";
+import Cors from "cors";
+
+// Inicializa el middleware CORS
+const cors = Cors({
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  origin: '*',  // Permite todas las fuentes, cámbialo a un dominio específico si lo necesitas
+});
+
+// Función para ejecutar el middleware de CORS
+const runMiddleware = (req: NextApiRequest, res: NextApiResponse, fn: any) =>
+  new Promise((resolve, reject) => {
+    fn(req, res, (result: any) =>
+      result instanceof Error ? reject(result) : resolve(result)
+    );
+  });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Ejecuta el middleware de CORS
+  await runMiddleware(req, res, cors);
+
   await dbConnect();
 
   try {
     if (req.method === "GET") {
-      try { 
+      try {
         const tasks: Array<typeof Task> = await Task.find();
-  
         res.status(200).json(tasks);
       } catch (error) {
         console.error("Error al obtener tareas:", error);
@@ -27,7 +44,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const newTask = await Task.create({
         title,
         description,
-        completed: !!completed
+        completed: !!completed,
       });
 
       return res.status(201).json(newTask);
